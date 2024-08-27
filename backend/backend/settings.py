badb5ba2e92413876bf3875bc197723e82c4e8d9
SOHINI MAJUMDAR <sohini.majumdar@salesforce.com>
2024-08-28 04:35:30 +0530

    Trying to get DB configuration work in both local and render hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -114,10 +114,18 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT")
+    }
 }
+# if prod environment
+if os.getenv('DATABASE_URL'):
+    DATABASES['default'] = dj_database_url.config(default=os.getenv('DATABASE_URL'))
 
 
 # Password validation
